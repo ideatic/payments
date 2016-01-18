e72475ecc49a3fba22930ea8182019f98963157c
Javier Marín <contacto@ideatic.net>
2016-01-18 15:14:23 +0100

    Improved error descriptions

--- Payment/Paypal.php
+++ Payment/Paypal.php
@@ -139,7 +139,7 @@ Si recibe la notificación "NO VÁLIDO", debe tratarla como sospechosa e investi
                            ->execute();
 
         if ($response->status() != 200 || strcasecmp($response->body(), 'VERIFIED') != 0) {
-            throw new Payment_Exception('Invalid Paypal response', $response);
+            throw new Payment_Exception('Invalid Paypal response: ' . $response->body());
         }
 
         if (isset($this->find_txnid_callback, $this->store_txnid_callback)) {
